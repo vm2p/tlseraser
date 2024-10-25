1cfe6eaa80d078db7aa7754979a7b7c8f9fa320f
briland <briland.hitaj90@gmail.com>
2024-10-25 10:05:05 -0400

    tls info script

---
+++ scripts/tls_info.py
@@ -0,0 +1,73 @@
+"""Extract TLS info from a given Website URL"""
+
+from __future__ import print_function
+import os
+import sys
+import argparse
+import time
+import logging
+
+import ssl
+import socket
+
+from rich.logging import RichHandler
+from rich import print
+
+FORMAT = "%(message)s"
+logging.basicConfig(level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()])
+
+log = logging.getLogger("rich")
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-u", "--url", 
+                        type=str, 
+                        default="", 
+                        help="target test service")
+    
+    parser.add_argument("-p", "--port", 
+                        type=int, 
+                        default=443, 
+                        help="target port")    
+
+    args = parser.parse_args()
+    return args
+
+
+def check_tls(target_host: str, target_port: int) -> str:
+    """Based on: https://medium.com/@sotiraq.sima/how-to-check-the-tls-of-your-website-with-python-1931a8abcf8e"""
+
+    tls_version = ""
+    cert_info = ""
+    context = ssl.create_default_context()
+    with socket.create_connection((target_host, target_port)) as sock:
+        with context.wrap_socket(sock, server_hostname=target_host) as ssock:
+            tls_version = ssock.version()
+            cert_info = ssock.getpeercert()
+    
+    sock.close()
+
+    return tls_version, cert_info
+
+
+def main():
+    start_time = time.time()
+    config = parse_arguments()
+    
+    if config.url != "":
+        log.info(f"Assessing: {config.url}")
+
+        tls_ver, ssl_cert_data = check_tls(target_host=config.url, target_port=config.port)
+        print(f"TLS Version for {config.url} using port: {config.port} is: {tls_ver}")
+        print(f"Certificate details:")
+        print(ssl_cert_data)
+    else:
+        log.error(f"Please provide a correct service path!")
+
+    log.info(f"Execution completed in {time.time() - start_time} seconds.")
+
+
+if __name__ == '__main__':
+    main()
